61300aa85e002ad31180cf170e7dfb5e10e095b5
Paweł Kmiecik <pwkmiecik@gmail.com>
2019-06-01 18:16:15 +0200

    05 added postprocessing

---
+++ src/main/java/pl/sdacademy/spring/beans/HelloPostProcessor.java
@@ -0,0 +1,19 @@
+package pl.sdacademy.spring.beans;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.lang.Nullable;
+
+public class HelloPostProcessor implements BeanPostProcessor {
+
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Postprocessing bean before init: " + beanName);
+        return bean;
+    }
+
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Postprocessing bean after init: " + beanName);
+        return bean;
+    }
+
+}
